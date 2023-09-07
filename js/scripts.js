60fef96a469e29a8f807725f1e6ad94800f94be2
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-09-07 17:47:58 -0400

    Add a new item to list of items

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 function newItem() {
+
+// 1. Adding a new item to the list of items:
+    let list = $('#list');
+    let inputValue = $('#input').val();
+    let li = $('<li></li>');
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        list.append(li);
+    }
+
 }
