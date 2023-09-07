5a04c82e08df18c990735eeecd29ce39809a195b
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-09-07 17:48:14 -0400

    Cross out an item from list of items

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,9 @@ function newItem() {
         list.append(li);
     }
 
+// 2. Crossing out an item from the list of items:
+    li.on('dblclick', function() {
+        li.toggleClass('strike');
+    });
+
 }
